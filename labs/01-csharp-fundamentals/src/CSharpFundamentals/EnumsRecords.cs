191c855b5fbda34068969b9ca9217d0b2cd10438
Pablo <pablo.santosx77@gmail.com>
2026-08-05 13:07:24 -0300

    fix: typos

--- labs/01-csharp-fundamentals/src/CSharpFundamentals/EnumsRecords.cs
+++ labs/01-csharp-fundamentals/src/CSharpFundamentals/EnumsRecords.cs
@@ -2,17 +2,17 @@ namespace CSharpFundamentals
 {
     enum EnemyState
     {
-        SEEKING,
-        ATTACKING,
-        RUNNIN,
-        THINKING
+        Seeking,
+        Atacking,
+        Running,
+        Thinking
     }
     public class EnumsRecords
     {
         public static void Run()
         {
-            var currentState = EnemyState.THINKING;
-            if (currentState == EnemyState.THINKING) Console.WriteLine("the enemy is thinking.");
+            var currentState = EnemyState.Thinking;
+            if (currentState == EnemyState.Thinking) Console.WriteLine("the enemy is thinking.");
         }
     }
 }
